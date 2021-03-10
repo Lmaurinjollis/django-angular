ac48094f6db43994531caea37a0b48b197706c0b
Ludwig Maurin-Jollis--Gally <ludwig.maurinjollisgally@outlook.fr>
2021-03-10 10:11:28 +0100

    Services and Components
    Generating service and component that'll be used

---
+++ frontend/src/app/add-thread/add-thread.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-add-thread',
+  templateUrl: './add-thread.component.html',
+  styleUrls: ['./add-thread.component.css']
+})
+export class AddThreadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AddThreadComponent } from './add-thread/add-thread.component';
+import { ThreadDetailComponent } from './thread-detail/thread-detail.component';
+import { ThreadListComponent } from './thread-list/thread-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddThreadComponent,
+    ThreadDetailComponent,
+    ThreadListComponent
   ],
   imports: [
     BrowserModule
