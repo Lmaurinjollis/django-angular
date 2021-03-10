f2c667e31093cccf10ffc347266d9dc30d982b1c
Ludwig Maurin-Jollis--Gally <ludwig.maurinjollisgally@outlook.fr>
2021-03-10 12:49:18 +0100

    Thread detail
    Details are now accessible and editing/deleting is now implemented.

--- frontend/src/app/thread-detail/thread-detail.component.ts
+++ frontend/src/app/thread-detail/thread-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ThreadService } from 'src/app/thread.service';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-thread-detail',
@@ -7,9 +9,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThreadDetailComponent implements OnInit {
 
-  constructor() { }
+  currentThread = null;
+  message = '';
 
-  ngOnInit(): void {
+  constructor(private threadService: ThreadService, private route: ActivatedRoute,
+              private router: Router) { }
+
+  ngOnInit() {
+    this.message = '';
+    this.getThread(this.route.snapshot.paramMap.get('id'));
+  }
+
+  getThread(id) {
+    this.threadService.get(id).subscribe(
+      data => {
+        this.currentThread = data;
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  updatePublished(status) {
+    const data = {
+      title: this.currentThread.title,
+      description: this.currentThread.description,
+      published: status
+    };
+
+    this.threadService.update(this.currentThread.id, data).subscribe(
+      response => {
+        this.currentThread.published = status;
+        console.log(response);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  updateThread() {
+    this.threadService.update(this.currentThread.id, this.currentThread).subscribe(
+      response => {
+        console.log(response);
+        this.message = 'The thread was updated successfully!';
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  deleteThread() {
+    this.threadService.delete(this.currentThread.id).subscribe(
+      response => {
+        console.log(response);
+        this.router.navigate(['/threads']);
+      },
+      error => {
+        console.log(error);
+      }
+    );
   }
 
 }
