f7cebb2fd3abe46616f642ba72c0fe2fb2c78d09
Ludwig Maurin-Jollis--Gally <ludwig.maurinjollisgally@outlook.fr>
2021-03-10 11:50:13 +0100

    Add thread
    The add-thread component is now linked to the django API

--- frontend/src/app/add-thread/add-thread.component.ts
+++ frontend/src/app/add-thread/add-thread.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ThreadService } from 'src/app/thread.service';
 
 @Component({
   selector: 'app-add-thread',
@@ -7,9 +8,42 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AddThreadComponent implements OnInit {
 
-  constructor() { }
+  thread = {
+    title: '',
+    description: '',
+    published: false
+  };
+  submitted = false;
+
+  constructor(private threadService: ThreadService) { }
 
   ngOnInit(): void {
+  }
+
+  saveThread(){
+    const data = {
+      title: this.thread.title,
+      description: this.thread.description
+    };
+
+    this.threadService.create(data).subscribe(
+      response => {
+        console.log(response);
+        this.submitted = true;
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  resetForm(){
+    this.submitted = false;
+    this.thread = {
+      title: '',
+      description: '',
+      published: false
+    };
   }
 
 }
