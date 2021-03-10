4611c73a1b15f1621e6f1aafbdf30c87d0ef9cf6
Ludwig Maurin-Jollis--Gally <ludwig.maurinjollisgally@outlook.fr>
2021-03-10 12:24:11 +0100

    Thread list
    Now the thread list appear on the front page and every thread can be deleted.

--- frontend/src/app/thread-list/thread-list.component.ts
+++ frontend/src/app/thread-list/thread-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ThreadService } from 'src/app/thread.service';
 
 @Component({
   selector: 'app-thread-list',
@@ -7,9 +8,48 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ThreadListComponent implements OnInit {
 
-  constructor() { }
+  threads: any;
+  currentThread = null;
+  currentIndex = -1;
+
+  constructor(private threadService: ThreadService) { }
 
   ngOnInit(): void {
+    this.getThreads();
   }
 
+  getThreads(){
+    this.threadService.getAll().subscribe(
+      data => {
+        this.threads = data;
+        console.log(data);
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+
+  refreshList() {
+    this.getThreads();
+    this.currentThread = null;
+    this.currentIndex = -1;
+  }
+
+  setActiveThread(thread, index) {
+    this.currentThread = thread;
+    this.currentIndex = index;
+  }
+
+  removeAllThreads() {
+    this.threadService.deleteAll().subscribe(
+      response => {
+        console.log(response);
+        this.getThreads();
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
 }
